b8bb1300f050e54b40967a40febae55d72c68cba
Smodger <chrisnorman1988@gmail.com>
2019-08-27 10:57:46 +0100

    wip

--- models/Lesson.js
+++ models/Lesson.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const lessonSchema = new Schema({
     dayOfTheWeek: {type : String},
     startHour: {type: Number, max: 24},
-    startMinutes: {type: Number, max: 60},
+    startMinutes: {type: String, match :/^[0-9]*$/},
     location: {type: String},
     yogaStyle: {type: String},
     linkToStudio : {type: String}
