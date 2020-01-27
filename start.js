826d500eaa32f947a9298f1c6a15b5f6e78dca9f
Smodger <chrisnorman1988@gmail.com>
2020-01-27 20:56:45 +0000

    added additional plugin and presets for babel compiling to help with ssr

--- start.js
+++ start.js
@@ -2,7 +2,29 @@
 // Preventing erros when SSR for SEO
 
 require("@babel/register")({
-  presets: ["@babel/preset-env"]
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+      [
+        "css-modules-transform",
+          {
+              camelCase: true,
+              extensions: [ ".css"],
+          }
+      ],
+      [
+        "@babel/plugin-proposal-class-properties",
+        {
+          "loose" : true
+        }
+      ],
+      [
+      "transform-assets",
+        {
+          "extensions": ["svg", "png", "jpg", "jpeg"]
+        }
+      ],
+      "dynamic-import-node",
+  ]
 });
 
 // Import app.
