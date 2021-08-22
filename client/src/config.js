b201584c014848915e968355dd384d0532c3e077
Chris Norman <chris.norman@lakeland.co.uk>
2021-08-22 21:57:29 +0100

    push

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,7 @@
 export const API_URL = process.env.REACT_APP_MONGODB_URI || "http://localhost:1234";
-
+export const GIT_TOKEN = process.env.GIT_TOKEN
 export const s3env = {
   bucket : process.env.REACT_APP_S3_BUCKET,
   region : process.env.REACT_APP_S3_REGION
 }
+
